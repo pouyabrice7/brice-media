5e9da79716df3fcc17cd247043849f9d326985e2
pouyabrice7 <pouya.brice7@gmail.com>
2026-08-05 15:53:58 +0330

    Implement news rendering in main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,78 @@
+/*====================================
+BRICE MEDIA
+AUTO NEWS RENDER SYSTEM
+====================================*/
 
+
+const newsContainer = document.getElementById("news-container");
+
+
+if(newsContainer){
+
+
+newsData.forEach(news => {
+
+
+const newsCard = document.createElement("div");
+
+
+newsCard.classList.add("news-card");
+
+
+
+newsCard.innerHTML = `
+
+<img src="${news.image}" alt="${news.title}">
+
+
+<div class="news-content">
+
+
+<span>
+${news.category}
+</span>
+
+
+<h3>
+${news.title}
+</h3>
+
+
+<p>
+${news.description}
+</p>
+
+
+<small>
+
+📅 ${news.date}
+&nbsp;&nbsp;
+✍ ${news.author}
+&nbsp;&nbsp;
+👁 ${news.views}
+
+</small>
+
+
+<br><br>
+
+
+<a href="#" class="btn">
+Read More
+</a>
+
+
+</div>
+
+`;
+
+
+
+newsContainer.appendChild(newsCard);
+
+
+
+});
+
+
+}
