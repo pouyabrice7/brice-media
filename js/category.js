bc5b3b2c44045be5e0e8b871bc7a2af926d8e1cf
pouyabrice7 <pouya.brice7@gmail.com>
2026-08-06 22:46:19 +0330

    Add news display for Technology category

---
+++ js/category.js
@@ -0,0 +1,45 @@
+const container = document.getElementById("news-container");
+
+if(container){
+
+newsData
+.filter(news => news.category === "Technology")
+.forEach(news => {
+
+container.innerHTML += `
+
+<div class="news-card">
+
+<img src="../${news.image}" alt="${news.title}">
+
+<div class="news-content">
+
+<span>${news.category}</span>
+
+<h3>${news.title}</h3>
+
+<p>${news.description}</p>
+
+<small>
+📅 ${news.date}
+&nbsp;
+✍ ${news.author}
+&nbsp;
+👁 ${news.views}
+</small>
+
+<br><br>
+
+<a href="../article.html?id=${news.id}" class="btn">
+Read More
+</a>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
