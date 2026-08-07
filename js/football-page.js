443e7031ee59b0abedaf30525337f7a855f18aeb
pouyabrice7 <pouya.brice7@gmail.com>
2026-08-08 01:55:14 +0330

    Add football news rendering to the page

---
+++ js/football-page.js
@@ -0,0 +1,65 @@
+const footballContainer = document.getElementById("football-container");
+
+
+if(footballContainer){
+
+
+footballNews.forEach(news=>{
+
+
+footballContainer.innerHTML += `
+
+
+<div class="news-card">
+
+
+<img src="${news.image}" alt="${news.title}">
+
+
+<div class="news-content">
+
+
+<span>
+${news.category}
+</span>
+
+
+<h3>
+${news.title}
+</h3>
+
+
+<p>
+${news.description}
+</p>
+
+
+<small>
+📅 ${news.date}
+&nbsp;
+✍ ${news.author}
+&nbsp;
+👁 ${news.views}
+</small>
+
+
+<br><br>
+
+
+<a href="../article.html?id=${news.id}" class="btn">
+Read More
+</a>
+
+
+</div>
+
+
+</div>
+
+
+`;
+
+});
+
+
+}
