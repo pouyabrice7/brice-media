3b41be6e10ea18e709518b222b6da56f9896babe
pouyabrice7 <pouya.brice7@gmail.com>
2026-08-08 01:23:15 +0330

    Display football news in news container

--- js/football.js
+++ js/football.js
@@ -121,3 +121,67 @@ content:"Technology continues changing how football matches are played, analyzed
 }
 
 ];
+
+const newsContainer = document.getElementById("news-container");
+
+
+if(newsContainer){
+
+
+footballNews.forEach(news=>{
+
+
+newsContainer.innerHTML += `
+
+<div class="news-card">
+
+
+<img src="../${news.image}" alt="${news.title}">
+
+
+<div class="news-content">
+
+
+<span>
+${news.category}
+</span>
+
+
+<h3>
+${news.title}
+</h3>
+
+
+<p>
+${news.description}
+</p>
+
+
+<small>
+📅 ${news.date}
+&nbsp;
+✍ ${news.author}
+&nbsp;
+👁 ${news.views}
+</small>
+
+
+<br><br>
+
+
+<a href="../article.html?id=${news.id}" class="btn">
+Read More
+</a>
+
+
+</div>
+
+
+</div>
+
+`;
+
+});
+
+
+}
