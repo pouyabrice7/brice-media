e44b8b8cb924758e9c0ae8995d178049e70b3a45
pouyabrice7 <pouya.brice7@gmail.com>
2026-08-07 22:51:50 +0330

    Update technology-page.js

--- js/technology-page.js
+++ js/technology-page.js
@@ -1,1 +1,66 @@
+const technologyContainer = document.getElementById("technology-container");
 
+
+if(technologyContainer){
+
+
+technologyNews.forEach(news=>{
+
+
+technologyContainer.innerHTML += `
+
+
+<div class="news-card">
+
+
+<img src="${news.image}" alt="${news.title}">
+
+
+<div class="news-content">
+
+
+<span>
+${news.category}
+</span>
+
+
+<h3>
+${news.title}
+</h3>
+
+
+<p>
+${news.description}
+</p>
+
+
+<small>
+📅 ${news.date}
+&nbsp;
+✍ ${news.author}
+&nbsp;
+👁 ${news.views}
+</small>
+
+
+<br><br>
+
+
+<a href="../article.html?id=${news.id}" class="btn">
+Read More
+</a>
+
+
+</div>
+
+
+</div>
+
+
+`;
+
+
+});
+
+
+}
