d9475631b2df6264068f7a7a3df2c141e2fc9c9c
pouyabrice7 <pouya.brice7@gmail.com>
2026-08-07 14:28:11 +0330

    Refactor category page news rendering logic

--- js/category.js
+++ js/category.js
@@ -1,69 +1,81 @@
+/*====================================
+BRICE MEDIA
+CATEGORY PAGE
+====================================*/
+
 const container = document.getElementById("news-container");
 
-alert("category loaded");
-alert(typeof newsData);
+if (!container) {
+    console.log("news-container not found");
+} else {
 
+    // نام پوشه فعلی (technology, sports, ...)
+    const currentCategory =
+        window.location.pathname
+        .split("/")
+        .filter(Boolean)
+        .pop()
+        .toLowerCase();
 
-if(container){
+    // تبدیل نام پوشه به نام دسته
+    const categoryMap = {
+        technology: "Technology",
+        sports: "Sports",
+        wealth: "Business",
+        celebrities: "Celebrities",
+        news: "News"
+    };
 
-newsData
-.filter(news => news.category === "Technology")
-.forEach(news => {
+    const categoryName = categoryMap[currentCategory];
 
+    const filteredNews = newsData.filter(news => news.category === categoryName);
 
-container.innerHTML += `
+    if (filteredNews.length === 0) {
 
-<div class="news-card">
+        container.innerHTML = `
+            <h2 style="text-align:center;padding:40px;color:#999;">
+                No News Available
+            </h2>
+        `;
 
-<img src="../${news.image}" alt="${news.title}">
+    } else {
 
+        filteredNews.forEach(news => {
 
-<div class="news-content">
+            container.innerHTML += `
+            <div class="news-card">
 
+                <img src="../${news.image}" alt="${news.title}">
 
-<span>${news.category}</span>
+                <div class="news-content">
 
+                    <span>${news.category}</span>
 
-<h3>
-${news.title}
-</h3>
+                    <h3>${news.title}</h3>
 
+                    <p>${news.description}</p>
 
-<p>
-${news.description}
-</p>
+                    <small>
+                        📅 ${news.date}
+                        &nbsp;&nbsp;
+                        ✍ ${news.author}
+                        &nbsp;&nbsp;
+                        👁 ${news.views}
+                    </small>
 
+                    <br><br>
 
-<small>
-📅 ${news.date}
-&nbsp;
-✍ ${news.author}
-&nbsp;
-👁 ${news.views}
-</small>
+                    <a href="../article.html?id=${news.id}" class="btn">
+                        Read More
+                    </a>
 
+                </div>
 
-<br><br>
+            </div>
+            `;
 
+        });
 
-<a href="../article.html?id=${news.id}" class="btn">
-Read More
-</a>
-
-
-</div>
-
-</div>
-
-`;
-
-
-});
-
-
-}
-else{
-
-console.log("news-container not found");
+    }
 
 }
